da00b44ac48c0a866d3f9de814331184759128e7
Constantine Ter-Matevosian <terkot1010@gmail.com>
2020-09-23 11:54:28 +0300

    Added SK-combinator transformer + a bunch of examples

--- src/LambdaCalculusEssentials.hs
+++ src/LambdaCalculusEssentials.hs
@@ -4,52 +4,72 @@ import Data.List ( (\\)
                  , union
                  )
 
-data LambdaTerm = Var    String 
-                | Apply  LambdaTerm LambdaTerm 
-                | Lambda String     LambdaTerm 
+infixl 5 :@:
+
+data LambdaTerm = Var String 
+                | LambdaTerm :@: LambdaTerm 
+                | L String LambdaTerm 
                 deriving Eq
 
 instance Show LambdaTerm where
-    show (Var s) = s
-    show (Apply p q) = "(" ++ show p ++ " " ++ show q ++ ")"
-    show (Lambda x p) = "(\\" ++ x ++ ". " ++ show p ++ ")"
+  show (Var s) = s
+  show (p :@: q) = "(" ++ show p ++ " " ++ show q ++ ")"
+  show (L x p) = "(\\" ++ x ++ ". " ++ show p ++ ")"
 
 infix 4 =@=
 (=@=) :: LambdaTerm -> LambdaTerm -> Bool
-(Var x) =@= (Var y) = x == y
-(Apply p q) =@= (Apply r s) = (p =@= r) && (q =@= s)
-(Lambda x p) =@= (Lambda y q) = substitute p x (Var "_RESERVED") =@= substitute q y (Var "_RESERVED")
-_ =@= _ = False
+p =@= q = go p q 0
+  where
+    go (Var x) (Var y) _ = x == y
+    go (p :@: q) (r :@: s) n = (go p q n) && (go r s n)
+    go (L x p) (L y q) n = go (substitute p x (Var $ "_RESERVED" ++ show n)) 
+                                        (substitute q y (Var $ "_RESERVED" ++ show n)) (n + 1)
+    go _ _ _ = False
 
 substitute :: LambdaTerm -> String -> LambdaTerm -> LambdaTerm
 substitute p@(Var y) x q = if x == y then q else p
-substitute (Apply r s) x q = Apply (substitute r x q) (substitute s x q)
-substitute p@(Lambda y r) x q = if x == y then p else Lambda y (substitute r x q)
+substitute (r :@: s) x q = (substitute r x q) :@: (substitute s x q)
+substitute p@(L y r) x q = if x == y 
+                                then p 
+                                else L y (substitute r x q)
+
+substituteLiterally :: LambdaTerm -> String -> String -> LambdaTerm
+substituteLiterally p@(Var y) x q = if x == y then Var q else p
+substituteLiterally (r :@: s) x q = (substituteLiterally r x q) :@: (substituteLiterally s x q)
+substituteLiterally p@(L y r) x q = if x == y 
+                                         then L q (substituteLiterally r x q)
+                                         else L y (substituteLiterally r x q)
 
 linkers :: LambdaTerm -> [String]
 linkers (Var _) = []
-linkers (Apply p q) = linkers p ++ linkers q
-linkers (Lambda x p) = [x] ++ linkers p
+linkers (p :@: q) = linkers p ++ linkers q
+linkers (L x p) = x : linkers p
 
 free :: LambdaTerm -> [String]
 free (Var x) = [x]
-free (Apply p q) = free p `union` free q
-free (Lambda x p) = free p \\ [x]
+free (p :@: q) = free p `union` free q
+free (L x p) = free p \\ [x]
 
 eval :: LambdaTerm -> LambdaTerm
 eval v@(Var x) = v
-eval (Lambda x p) = Lambda x (eval p)
-eval (Apply p@(Lambda x p') q) = substitute p' x q
-eval (Apply p q) = Apply (eval p) (eval q)
+eval (L x p) = L x (eval p)
+eval (p@(L x p') :@: q) 
+  = let p'Linkers = linkers p'
+        qEverything = linkers q ++ free q
+        newp'Linkers = (\(a, b, c) -> reverse a)
+                     $ foldl (\(acc, qE, n) x -> if x `elem` qE
+                                                 then ((x ++ show n) : acc, qE, n + 1)
+                                                 else (x : acc, qE, n + 1)
+                             ) ([], qEverything, 0) p'Linkers
+        newP = L x $ helperSub p' p'Linkers newp'Linkers
+    in if newP == p
+       then substitute p' x q
+       else newP :@: q
+  where
+    helperSub p []     []     = p
+    helperSub p (x:xs) (y:ys) = helperSub (substituteLiterally p x y) xs ys
 
-i :: LambdaTerm
-i = Lambda "x" (Var "x")
-
-k :: LambdaTerm
-k = Lambda "x" (Lambda "y" (Var "x"))
-
-s :: LambdaTerm
-s = Lambda "x" (Lambda "y" (Lambda "z" (Apply (Apply (Var "x") (Var "z")) (Apply (Var "y") (Var "z")))))
+eval (p :@: q) = (eval p) :@: (eval q)
 
 reduce :: LambdaTerm -> LambdaTerm
 reduce a = let a' = eval a
@@ -73,28 +93,89 @@ reduceVerbose' a = let a' = eval a
 
 churchNumeral :: Int -> LambdaTerm
 churchNumeral n
-    | n < 0 = error "church numerals can only be non-negative"
-    | otherwise = Lambda "f" (Lambda "x" (construct n))
+  | n < 0 = error "church numerals can only be non-negative"
+  | otherwise = L "f" (L "x" (construct n))
   where
     construct :: Int -> LambdaTerm
     construct 0 = Var "x"
-    construct n = Apply (Var "f") $ construct (n - 1)
+    construct n = (Var "f") :@: construct (n - 1)
+
+-- examples --
+
+i :: LambdaTerm
+i = L "x" (Var "x")
+
+k :: LambdaTerm
+k = L "x" (L "y" (Var "x"))
+
+s :: LambdaTerm
+s = L "x" (L "y" (L "z" ((Var "x") :@: (Var "z") :@: ((Var "y") :@: (Var "z")))))
+
+i' :: LambdaTerm
+i' = Var "I"
+
+k' :: LambdaTerm
+k' = Var "K"
+
+s' :: LambdaTerm
+s' = Var "S"
 
 true :: LambdaTerm
-true = Lambda "a" (Lambda "b" (Var "a"))
+true = L "a" (L "b" (Var "a"))
 
 false :: LambdaTerm
-false = Lambda "a" (Lambda "b" (Var "b"))
+false = L "a" (L "b" (Var "b"))
 
 skk :: LambdaTerm
-skk = Apply (Apply s k) k
+skk = s :@: k :@: k
+
+skk' :: LambdaTerm
+skk' = Var "S" :@: Var "K" :@: Var "K"
 
 switch :: LambdaTerm
-switch = Apply (Apply s (Apply k (Apply s i))) (Apply (Apply s (Apply k k)) i)
+switch = (s :@: (k :@: (s :@: i))) :@: (s :@: (k :@: k) :@: i)
 
-y = Lambda "f" (Apply (Lambda "x" (Apply (Var "f") (Apply (Var "x") (Var "x")))) 
-                      (Lambda "x" (Apply (Var "f") (Apply (Var "x") (Var "x")))))
+y = L "f" ((L "x" ((Var "f") :@: ((Var "x") :@: (Var "x")))) :@:
+           (L "x" ((Var "f") :@: ((Var "x") :@: (Var "x")))))
 
 turing :: LambdaTerm
-turing = Apply (Lambda "x" (Lambda "y" (Apply (Var "y") (Apply (Apply (Var "x") (Var "x")) (Var "y")))))
-               (Lambda "x" (Lambda "y" (Apply (Var "y") (Apply (Apply (Var "x") (Var "x")) (Var "y")))))
+turing = (L "x" (L "y" ((Var "y") :@: ((Var "x") :@: (Var "x") :@: (Var "y")))))
+     :@: (L "x" (L "y" ((Var "y") :@: ((Var "x") :@: (Var "x") :@: (Var "y")))))
+
+mkPair :: LambdaTerm
+mkPair = L "a" (L "b" (L "x" ((Var "x") :@: (Var "a") :@: (Var "b"))))
+
+prL :: LambdaTerm
+prL = L "p" ((Var "p") :@: true)
+
+prR :: LambdaTerm
+prR = L "p" ((Var "p") :@: false)
+
+case' :: LambdaTerm
+case' = L "l" (L "r" (L "c" ((Var "c") :@: (Var "l") :@: (Var "r"))))
+
+inL :: LambdaTerm
+inL = L "l" (L "x" (L "y" ((Var "x") :@: (Var "l"))))
+
+inR :: LambdaTerm
+inR = L "r" (L "x" (L "y" ((Var "y") :@: (Var "r"))))
+
+-- SK-basis constructor --
+
+toSK :: LambdaTerm -> LambdaTerm
+toSK (Var x) = Var x
+toSK (p :@: q) = (toSK p) :@: (toSK q)
+toSK (L x (Var p)) = if x == p
+                     then skk
+                     else k :@: (Var p)
+toSK (L x (l@(L _ _))) = toSK (L x (toSK l))
+toSK (L x (p :@: q)) = s :@: (toSK (L x p)) :@: (toSK (L x q))
+
+toSK' :: LambdaTerm -> LambdaTerm
+toSK' (Var x) = Var x
+toSK' (p :@: q) = (toSK' p) :@: (toSK' q)
+toSK' (L x (Var p)) = if x == p
+                     then skk'
+                     else k' :@: (Var p)
+toSK' (L x (l@(L _ _))) = toSK' (L x (toSK' l))
+toSK' (L x (p :@: q)) = s' :@: (toSK' (L x p)) :@: (toSK' (L x q))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+module Spec where
+
+import Test.Hspec
+import LambdaCalculusEssentials
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Basic combinator tests" $ do
+        it "SKK-combinator is I-combinator" $ do
+            (reduce skk =@= i) `shouldBe` True
